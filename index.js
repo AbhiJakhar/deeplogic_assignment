6ef35e27bb186d1a229808c36cc8f43db8d9b315
AbhiJakhar <109135918+AbhiJakhar@users.noreply.github.com>
2024-03-07 21:03:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,5 +48,5 @@ function extractStories(html) {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}/getTimeStories`);
 });
